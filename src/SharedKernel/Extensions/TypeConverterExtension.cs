de6ba96faf84d43b68874cf5b606092ae6a85d09
Minh Sáng <minhsang.1mil@gmail.com>
2025-11-07 18:51:28 +0700

    refactor: update cases for TypeConverterExtension

--- src/SharedKernel/Extensions/TypeConverterExtension.cs
+++ src/SharedKernel/Extensions/TypeConverterExtension.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Globalization;
 using deniszykov.TypeConversion;
 using SharedKernel.Extensions.Reflections;
 
@@ -31,15 +32,172 @@ public static class TypeConverterExtension
         }
 
         Type underlyingTargetType = Nullable.GetUnderlyingType(targetType) ?? targetType;
-        if (underlyingTargetType == typeof(DateTime))
-        {
-            return Convert.ChangeType(input, underlyingTargetType);
-        }
-
         if (underlyingTargetType == typeof(Ulid))
         {
             Ulid ulid = input == null ? Ulid.Empty : Ulid.Parse(input.ToString());
             return ulid;
+        }
+
+        // DateOnly
+        if (underlyingTargetType == typeof(DateOnly))
+        {
+            // string inputs
+            if (input is string s)
+            {
+                s = s.Trim();
+
+                if (DateOnly.TryParse(s, out DateOnly d1))
+                {
+                    return d1;
+                }
+
+                if (DateTimeOffset.TryParse(s, out DateTimeOffset dto))
+                {
+                    return DateOnly.FromDateTime(dto.Date);
+                }
+
+                if (
+                    DateTime.TryParse(
+                        s,
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.AllowWhiteSpaces | DateTimeStyles.AssumeLocal,
+                        out DateTime dts
+                    )
+                )
+                {
+                    return DateOnly.FromDateTime(dts);
+                }
+
+                if (long.TryParse(s, out long unix))
+                {
+                    DateTimeOffset dto2 =
+                        s.Length >= 13
+                            ? DateTimeOffset.FromUnixTimeMilliseconds(unix)
+                            : DateTimeOffset.FromUnixTimeSeconds(unix);
+                    return DateOnly.FromDateTime(dto2.Date);
+                }
+
+                throw new InvalidCastException($"Cannot convert '{input}' to DateOnly.");
+            }
+
+            if (input is DateOnly d)
+            {
+                return d;
+            }
+
+            if (input is DateTime dt)
+            {
+                return DateOnly.FromDateTime(dt);
+            }
+
+            if (input is DateTimeOffset dtoInput)
+            {
+                return DateOnly.FromDateTime(dtoInput.Date);
+            }
+
+            if (input is long l)
+            {
+                DateTimeOffset dto =
+                    l >= 1_000_000_000_000
+                        ? DateTimeOffset.FromUnixTimeMilliseconds(l)
+                        : DateTimeOffset.FromUnixTimeSeconds(l);
+
+                return DateOnly.FromDateTime(dto.UtcDateTime);
+            }
+
+            if (input is int i)
+            {
+                var dto = DateTimeOffset.FromUnixTimeSeconds(i);
+                return DateOnly.FromDateTime(dto.UtcDateTime);
+            }
+
+            throw new InvalidCastException($"Cannot convert '{input}' to DateOnly.");
+        }
+
+        if (underlyingTargetType == typeof(DateTime))
+        {
+            if (input is string s)
+            {
+                if (long.TryParse(s, out long unixLong))
+                {
+                    var dto2 =
+                        s.Length >= 13
+                            ? DateTimeOffset.FromUnixTimeMilliseconds(unixLong)
+                            : DateTimeOffset.FromUnixTimeSeconds(unixLong);
+                    return dto2.UtcDateTime;
+                }
+                return Convert.ChangeType(input, typeof(DateTime));
+            }
+            if (input is DateTime dtValue)
+            {
+                return dtValue;
+            }
+
+            if (input is DateTimeOffset dtoValue)
+            {
+                return dtoValue.UtcDateTime;
+            }
+
+            if (input is DateOnly dOnly)
+            {
+                return dOnly.ToDateTime(TimeOnly.MinValue, DateTimeKind.Local);
+            }
+
+            if (input is long l)
+            {
+                DateTimeOffset dto =
+                    l >= 1_000_000_000_000
+                        ? DateTimeOffset.FromUnixTimeMilliseconds(l)
+                        : DateTimeOffset.FromUnixTimeSeconds(l);
+
+                return dto.UtcDateTime;
+            }
+
+            if (input is int i)
+            {
+                var dto = DateTimeOffset.FromUnixTimeSeconds(i);
+                return dto.UtcDateTime;
+            }
+
+            throw new InvalidCastException($"Cannot convert '{input.GetType()}' to DateTime.");
+        }
+
+        if (underlyingTargetType == typeof(DateTimeOffset))
+        {
+            if (input is string s)
+            {
+                if (long.TryParse(s, out long unixLong))
+                {
+                    var dto2 =
+                        s.Length >= 13
+                            ? DateTimeOffset.FromUnixTimeMilliseconds(unixLong)
+                            : DateTimeOffset.FromUnixTimeSeconds(unixLong);
+                    return dto2;
+                }
+            }
+            if (input is DateOnly dOnly)
+            {
+                return new DateTimeOffset(dOnly.ToDateTime(TimeOnly.MinValue, DateTimeKind.Local));
+            }
+
+            if (input is long l)
+            {
+                var dto =
+                    l >= 1_000_000_000_000
+                        ? DateTimeOffset.FromUnixTimeMilliseconds(l)
+                        : DateTimeOffset.FromUnixTimeSeconds(l);
+                return dto;
+            }
+
+            if (input is int i)
+            {
+                var dto = DateTimeOffset.FromUnixTimeSeconds(i);
+                return dto;
+            }
+
+            throw new InvalidCastException(
+                $"Cannot convert '{input.GetType()}' to DateTimeOffset."
+            );
         }
 
         var conversionProvider = new TypeConversionProvider();
